b753cdf5f362e12782ba5fee8d0f36c659ed253d
张新 <zhangxin@zhangxindemacbook-pro.local>
2016-09-14 23:05:29 +0800

    Initial Commit

---
+++ WeexDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeexDemo
+//
+//  Created by 张新 on 16/9/14.
+//  Copyright © 2016年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeexDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeexDemo
+//
+//  Created by 张新 on 16/9/14.
+//  Copyright © 2016年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
